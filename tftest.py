8316d3a4147d3961d253664b6937dd57700a60d1
PolarHawkins <polarhawkins@gmail.com>
2017-07-08 18:32:37 +0800

    first commit

--- tftest.py
+++ tftest.py
@@ -11,6 +11,7 @@ node3 = tf.add(node1,node2)
 print("node3:",node3)
 print("sess.run(node3):",sess.run(node3))
 
+
 a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
 adder_node = a + b
